ee61d702db221db3e30775b74b2e8c040db24c68
Carlos De la Cruz <carlosdlc.dev@gmail.com>
2024-12-08 23:00:20 -0500

    theme togglefix

--- src/components/theme-toggle-button.jsx
+++ src/components/theme-toggle-button.jsx
@@ -47,7 +47,7 @@ export default function ThemeToggle() {
         <>
             <Button
                 variant="ghost"
-                className="group/toggle h-8 w-8 px-0 text-background dark:text-foreground/80 hover:bg-gray-100 dark:hover:bg-zinc-700/70"
+                className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent dark:text-accent-foreground hover:text-accent-foreground py-2 group/toggle h-8 w-8 px-0"
                 onClick={toggleTheme}
             >
                 <Sun className="hidden [html.dark_&]:block" />
