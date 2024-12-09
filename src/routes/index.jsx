550796d330817edf8a9082871c653f20eed13ff7
Carlos De la Cruz <carlosdlc.dev@gmail.com>
2024-12-09 00:06:03 -0500

    sign up

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -79,7 +79,7 @@ export default function Home() {
                             </p>
                         </div>
                         <Button asChild>
-                            <Link className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300" to="#">
+                            <Link className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300" to="/sign-up">
                                 Sign Up Now <ArrowRightIcon className="ml-2 h-4 w-4" />
                             </Link>
                         </Button>
